34343370a04c916aae3193613f6028cef877d942
scaramallion <scaram@gmail.com>
2016-02-16 14:57:42 +1100

    --quiet option functionality added

--- pynetdicom/apps/echoscu/echoscu.py
+++ pynetdicom/apps/echoscu/echoscu.py
@@ -44,16 +44,14 @@ def _setup_argparser():
     gen_opts.add_argument("--version", 
                           help="print version information and exit", 
                           action="store_true")
-    gen_opts.add_argument("--arguments", 
-                          help="print expanded command line arguments", 
-                          action="store_true")
-    gen_opts.add_argument("-q", "--quiet", 
+    output = gen_opts.add_mutually_exclusive_group()
+    output.add_argument("-q", "--quiet", 
                           help="quiet mode, print no warnings and errors", 
                           action="store_true")
-    gen_opts.add_argument("-v", "--verbose", 
+    output.add_argument("-v", "--verbose", 
                           help="verbose mode, print processing details", 
                           action="store_true")
-    gen_opts.add_argument("-d", "--debug", 
+    output.add_argument("-d", "--debug", 
                           help="debug mode, print debug information", 
                           action="store_true")
     gen_opts.add_argument("-ll", "--log-level", metavar='[l]', 
@@ -103,10 +101,88 @@ def _setup_argparser():
     net_opts.add_argument("--abort", 
                           help="abort association instead of releasing it", 
                           action="store_true")
+                          
+    # TLS Options
+    tls_opts = parser.add_argument_group('Transport Layer Security (TLS) Options')
+    tls_opts.add_argument("-dtls", "--disable-tls",
+                          help="use normal TCP/IP connection (default)", 
+                          action="store_true")
+    tls_opts.add_argument("-tls", "--enable-tls", 
+                          metavar="[p]rivate key file, [c]erficiate file",
+                          help="use authenticated secure TLD connection", 
+                          type=str)
+    tls_opts.add_argument("-tla", "--anonymous-tls",
+                          help="use secure TLD connection without certificate", 
+                          action="store_true")
+    tls_opts.add_argument("-ps", "--std-password",
+                          help="prompt user to type password on stdin (default)", 
+                          action="store_true")
+    tls_opts.add_argument("-pw", "--use-password", metavar="[p]assword",
+                          help="use specified password", 
+                          type=str)
+    tls_opts.add_argument("-nw", "--null-password",
+                          help="use empty string as password", 
+                          action="store_true")
+    tls_opts.add_argument("-pem", "--pem-keys",
+                          help="read keys and certificates as PEM file "
+                                                                    "(default)", 
+                          action="store_true")
+    tls_opts.add_argument("-der", "--der-keys",
+                          help="read keys and certificates as DER file", 
+                          action="store_true")
+    tls_opts.add_argument("-cf", "--add-cert-file", 
+                          metavar="[c]ertificate filename",
+                          help="add certificate file to list of certificates", 
+                          type=str)
+    tls_opts.add_argument("-cd", "--add-cert-dir", 
+                          metavar="[c]ertificate directory",
+                          help="add certificates in d to list of certificates", 
+                          type=str)
+    tls_opts.add_argument("-cs", "--cipher", 
+                          metavar="[c]iphersuite name",
+                          help="add ciphersuite to list of negotiated suites", 
+                          type=str)
+    tls_opts.add_argument("-dp", "--dhparam", 
+                          metavar="[f]ilename",
+                          help="read DH parameters for DH/DSS ciphersuites", 
+                          type=str)
+    tls_opts.add_argument("-rs", "--seed", 
+                          metavar="[f]ilename",
+                          help="seed random generator with contents of f", 
+                          type=str)
+    tls_opts.add_argument("-ws", "--write-seed", 
+                          help="write back modified seed (only with --seed)", 
+                          action="store_true")
+    tls_opts.add_argument("-wf", "--write-seed-file", 
+                          metavar="[f]ilename",
+                          help="write modified seed to file f", 
+                          type=str)
+    tls_opts.add_argument("-rc", "--require-peer-cert", 
+                          help="verify peer certificate, fail if absent "
+                                        "(default)", 
+                          action="store_true")
+    tls_opts.add_argument("-vc", "--verify-peer-cert", 
+                          help="verify peer certificate if present", 
+                          action="store_true")
+    tls_opts.add_argument("-ic", "--ignore-peer-cert", 
+                          help="don't verify peer certificate", 
+                          action="store_true")
 
     return parser.parse_args()
 
 args = _setup_argparser()
+
+if args.quiet:
+    for h in logger.handlers:
+        logger.removeHandler(h)
+        
+    logger.addHandler(logging.NullHandler())
+    
+    pynetdicom_logger = logging.getLogger('pynetdicom')
+    for h in pynetdicom_logger.handlers:
+        pynetdicom_logger.removeHandler(h)
+        
+    pynetdicom_logger.addHandler(logging.NullHandler())
 
 if args.verbose:
     logger.setLevel(logging.INFO)
@@ -118,26 +194,37 @@ if args.debug:
     pynetdicom_logger = logging.getLogger('pynetdicom')
     pynetdicom_logger.setLevel(logging.DEBUG)
 
-logger.debug('$echoscu.py v%s %s $' %('0.1.0', '2016-02-10'))
+logger.debug('$echoscu.py v%s %s $' %('0.2.0', '2016-02-16'))
 logger.debug('')
 
 called_ae = {'AET' : args.called_aet, 
              'Address' : args.peer, 
              'Port' : args.port}
 
-# Create application entity
+# Create local AE
 # Binding to port 0, OS will pick an available port
-ae = AE(args.calling_aet, 0, [VerificationSOPClass], [], 
-        SupportedTransferSyntax=[ExplicitVRLittleEndian])
+ae = AE(AET=args.calling_aet, 
+        port=0, 
+        SOPSCU=[VerificationSOPClass], 
+        SOPSCP=[], 
+        SupportedTransferSyntax=[ExplicitVRLittleEndian],
+        MaxPDULength=args.max_pdu)
 
-# Request association with remote
-assoc = ae.request_association(args.peer, args.port, args.called_aet)
+# Request association with remote AE
+assoc = ae.request_association(args.peer, 
+                               args.port, 
+                               args.called_aet)
 
 if assoc.Established:
-    status = assoc.send_c_echo()
     
-    # Release association
-    assoc.Release()
+    for ii in args.repeat:
+        status = assoc.send_c_echo()
+    
+    # Abort or release association
+    if args.abort:
+        assoc.Abort()
+    else:
+        assoc.Release()
 
 # Quit
 ae.Quit()
