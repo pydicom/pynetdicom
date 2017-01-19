cea1d418029f869a45199c20fd5f3a68e5e1d740
scaramallion <scaramallion@users.noreply.github.com>
2017-01-19 12:10:09 +1100

    Docstring updates, changed is_implicit_vr and is_little_endian parameters in encode_element to optional, renamed some variables.

--- pynetdicom3/dsutils.py
+++ pynetdicom3/dsutils.py
@@ -9,61 +9,60 @@ from pydicom.filewriter import write_dataset, write_data_element
 
 LOGGER = logging.getLogger('pynetdicom3.dsutils')
 
-def decode(bytestring, is_implicit_VR, is_little_endian):
-    """Decode a bytestream to a pydicom Dataset.
+def decode(bytestring, is_implicit_vr, is_little_endian):
+    """Decode `bytestring` to a pydicom Dataset.
 
     When sent a DIMSE Message from a peer AE, decode the data and convert
-    it to a pydicom Dataset instance
+    it to a pydicom Dataset instance.
 
     Parameters
     ----------
-    b - io.BytesIO
-        The DIMSE Message sent from the peer AE
-    is_implicit_VR - bool
-        Is implicit or explicit VR
-    is_little_endian - bool
-        The byte ordering, little or big endian
+    byestring : io.BytesIO
+        The encoded dataset in the DIMSE Message sent from the peer AE.
+    is_implicit_vr : bool
+        The dataset is encoded as implicit or explicit VR.
+    is_little_endian : bool
+        The byte ordering of the encoded dataset, little or big endian.
 
     Returns
     -------
-    pydicom.Dataset
-        The Message contents decoded into a Dataset
+    pydicom.dataset.Dataset
+        The decoded dataset.
     """
-    if is_little_endian:
-        transfer_syntax = "Little Endian"
-    else:
-        transfer_syntax = "Big Endian"
-
-    if is_implicit_VR:
+    ## Logging
+    transfer_syntax = "Little Endian" if is_little_endian else "Big Endian"
+    if is_implicit_vr:
         transfer_syntax += " Implicit"
     else:
         transfer_syntax += " Explicit"
 
-    LOGGER.debug('pydicom::read_dataset() TransferSyntax="%s"', transfer_syntax)
+    LOGGER.debug('pydicom.read_dataset() TransferSyntax="%s"', transfer_syntax)
 
+    ## Decode the dataset
     # Rewind to the start of the stream
     bytestring.seek(0)
-    return read_dataset(bytestring, is_implicit_VR, is_little_endian)
+    return read_dataset(bytestring, is_implicit_vr, is_little_endian)
 
-def encode(ds, is_implicit_VR, is_little_endian):
-    """Encode a pydicom Dataset to a byte stream.
+def encode(ds, is_implicit_vr, is_little_endian):
+    """Encode a pydicom Dataset `ds` to a byte stream.
 
     Parameters
     ----------
-    ds - pydicom.dataset.Dataset
+    ds : pydicom.dataset.Dataset
         The dataset to encode
-    is_implicit_VR - bool
-        Transfer syntax implicit/explicit VR
-    is_little_endian - bool
-        Transfer syntax byte ordering
+    is_implicit_vr : bool
+        The element encoding scheme the dataset will be encoded with.
+    is_little_endian : bool
+        The byte ordering the dataset will be encoded in.
 
     Returns
     -------
     bytes or None
-        The encoded dataset (if successful), None if encoding failed.
+        The encoded dataset (if successful), None if the encoding failed.
     """
+    # pylint: disable=broad-except
     fp = DicomBytesIO()
-    fp.is_implicit_VR = is_implicit_VR
+    fp.is_implicit_VR = is_implicit_vr
     fp.is_little_endian = is_little_endian
     try:
         write_dataset(fp, ds)
@@ -73,18 +72,36 @@ def encode(ds, is_implicit_VR, is_little_endian):
         fp.close()
         return None
 
-    rawstr = fp.parent.getvalue()
+    bytestring = fp.parent.getvalue()
     fp.close()
 
-    return rawstr
+    return bytestring
 
-def encode_element(elem, is_implicit_VR, is_little_endian):
-    """Encode a pydicom DataElement to a byte stream."""
+def encode_element(elem, is_implicit_vr=True, is_little_endian=True):
+    """Encode a pydicom DataElement `elem` to a byte stream.
+
+    The default is to encode the element as implicit VR little endian.
+
+    Parameters
+    ----------
+    elem : pydicom.dataelem.DataElement
+        The element to encode
+    is_implicit_vr : bool, optional
+        The element encoding scheme the element will be encoded with, default
+        is True.
+    is_little_endian : bool, optional
+        The byte ordering the element will be encoded in, default is True.
+
+    Returns
+    -------
+    bytes
+        The encoded element.
+    """
     fp = DicomBytesIO()
-    fp.is_implicit_VR = is_implicit_VR
+    fp.is_implicit_VR = is_implicit_vr
     fp.is_little_endian = is_little_endian
     write_data_element(fp, elem)
-    rawstr = fp.parent.getvalue()
+    bytestring = fp.parent.getvalue()
     fp.close()
 
-    return rawstr
+    return bytestring
