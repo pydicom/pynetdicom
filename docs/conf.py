558a150a2396966cc5a4c3ba2fc8276e46b2405f
scaramallion <scaramallion@users.noreply.github.com>
2018-04-28 15:01:09 +1000

    Add pydicom to setup.py

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ author = u'scaramallion'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'1.0'
+release = u'0.10.0'
 
 
 # -- General configuration ---------------------------------------------------
