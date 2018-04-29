45cd669361bc8ffeedeeab5b1e82a88b38af4689
scaramallion <scaramallion@users.noreply.github.com>
2018-04-29 18:32:27 +1000

    Use sphinx.ext.napoleon over numpydoc

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ extensions = [
     'sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx',
     'sphinx.ext.todo', 'sphinx.ext.imgmath', 'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode', 'sphinx_gallery.gen_gallery',
-    'sphinx.ext.autosummary', 'numpydoc',
+    'sphinx.ext.autosummary', 'sphinx.ext.napoleon',
     'sphinx_issues', 'sphinx.ext.linkcode'
 ]
 
