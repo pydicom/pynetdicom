5ab36b065a65d2e622c2434bc6e02e8e9f6e6d35
scaramallion <scaramallion@users.noreply.github.com>
2018-04-29 18:38:27 +1000

    Fix config

--- build_docs/circle/build_doc.sh
+++ build_docs/circle/build_doc.sh
@@ -110,7 +110,7 @@ conda create -n $CONDA_ENV_NAME --yes --quiet python=3
 source activate $CONDA_ENV_NAME
 
 conda install --yes --quiet pip setuptools numpy matplotlib sphinx \
-      pillow sphinx_rtd_theme numpydoc
+      pillow sphinx_rtd_theme numpydoc sphinxcontrib-napoleon
 conda install --yes --quiet -c conda-forge gdcm
 pip install sphinx-gallery
 
