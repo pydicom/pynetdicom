05d495eda94ee697892c4c73ea4e8fcc80f70a7e
scaramallion <scaramallion@users.noreply.github.com>
2018-05-31 10:25:23 +1000

    Fix apps

--- pynetdicom3/__init__.py
+++ pynetdicom3/__init__.py
@@ -1,17 +1,23 @@
 """
 Set module shortcuts and globals
 """
+from pydicom.uid import UID
+
 version = ['0', '9', '1']
 __version__ = '.'.join(version)
 
 # Encoded as SH, 16 bytes maximum
+# TODO: UPPER_CASE, change to PYNETDICOM_IMPLEMENTATION_VERSION
 pynetdicom_version = 'PYNETDICOM3_' + ''.join(version)
 assert 1 <= len(pynetdicom_version) <= 16
 
 # UID prefix provided by https://www.medicalconnections.co.uk/Free_UID
 # Encoded as UI, 64 bytes maximum
-pynetdicom_uid_prefix = '1.2.826.0.1.3680043.9.3811.' + '.'.join(version)
-assert len(pynetdicom_uid_prefix) <= 64
+# TODO: UPPER_CASE, change to PYNETDICOM_UID_PREFIX
+pynetdicom_uid_prefix = '1.2.826.0.1.3680043.9.3811.'
+# TODO: UPPER_CASE, change to PYNETDICOM_IMPLEMENTATION_UID
+pynetdicom_implementation_uid = UID(pynetdicom_uid_prefix + '.'.join(version))
+assert pynetdicom_implementation_uid.is_valid
 
 import logging
 
