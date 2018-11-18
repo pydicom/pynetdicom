73a14779ac6e84fd956cd7daf9b62010be290c65
scaramallion <scaramallion@users.noreply.github.com>
2018-11-18 12:48:37 +1100

    Simplify version functions

--- pynetdicom3/_version.py
+++ pynetdicom3/_version.py
@@ -1,14 +1,10 @@
 """Version information for pynetdicom3 based on PEP396 and 440.
 
-`VERSION_PATTERN`, `_parse_letter_version` and parts of `extract_components`
-are taken from the pypa packaging project (https://github.com/pypa/packaging)
-which is dual licensed:
+Parts of extract_components` are taken from the pypa packaging project
+(https://github.com/pypa/packaging) which is dual licensed:
 > This file is dual licensed under the terms of the Apache License, Version
 > 2.0, and the BSD License. See the LICENSE file in the root of (the pypa)
 > repository for complete details.
-
-The Apache License v2.0 is available at
-    https://www.apache.org/licenses/LICENSE-2.0
 """
 
 import re
@@ -58,28 +54,6 @@ def is_canonical(version):
     return match is not None
 
 
-def _parse_letter_version(letter, number):
-    """Parse the version component `letter` and `number`.
-
-    Parameters
-    ----------
-    letter : str or None
-    number : str or None
-
-    Returns
-    -------
-    tuple or None
-        Returns None if letter is None, otherwise returns a tuple containing
-        the parsed component in a format that meets PEP440 for pre-release,
-        post-release and dev components.
-    """
-    if letter:
-        # We normalize any letters to their lower case form
-        letter = letter.lower()
-
-        return letter, int(number)
-
-
 def extract_components(version):
     """Return the components from `version` as a dict"""
     if not is_canonical(version):
@@ -93,21 +67,25 @@ def extract_components(version):
     )
     match = _regex.search(version)
 
+    _pre = None
+    if match.group("pre_l"):
+        _pre = (match.group("pre_l"), int(match.group("pre_n")))
+
+    _post = None
+    if match.group("post_l"):
+        _post = (match.group("post_l"),
+                 int(match.group("post_n1") or match.group("post_n2")))
+
+    _dev = None
+    if match.group("dev_l"):
+        _dev = (match.group("dev_l"), int(match.group("dev_n")))
+
     components = {
         'epoch' : int(match.group("epoch")) if match.group("epoch") else 0,
-        'release' : tuple(int(i) for i in match.group("release").split(".")),
-        'pre-release' : _parse_letter_version(
-            match.group("pre_l"),
-            match.group("pre_n")
-        ),
-        'post-release' : _parse_letter_version(
-            match.group("post_l"),
-            match.group("post_n1") or match.group("post_n2")
-        ),
-        'dev' : _parse_letter_version(
-            match.group("dev_l"),
-            match.group("dev_n")
-        ),
+        'release' : tuple(int(ii) for ii in match.group("release").split(".")),
+        'pre' : _pre,
+        'post' : _post,
+        'dev' : _dev,
         'local' : match.group("local"),
     }
 
