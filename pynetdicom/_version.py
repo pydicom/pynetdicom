efac18fabc5e9f16ca01b45f3c7bdab6d634e59e
scaramallion <scaramallion@users.noreply.github.com>
2019-01-09 17:22:40 +1100

    Update version to release

--- pynetdicom/_version.py
+++ pynetdicom/_version.py
@@ -92,6 +92,6 @@ def extract_components(version):
     return components
 
 
-__version__ = '1.1.0.dev0'
+__version__ = '1.1.0'
 assert is_canonical(__version__)
 __version_info__ = extract_components(__version__)
