55e1b89bd20a7055ec92cc5736ca14edbe09e06e
scaramallion <scaramallion@users.noreply.github.com>
2019-01-10 11:02:13 +1100

    Update travis builds (#277)

---
+++ build_tools/travis/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+pip install codecov
+pip install coverage
+pip install pytest-cov
+pip install pytest
+pip install asv
+#python setup.py install
+
+echo ""
+echo "Test suite is " $TEST_SUITE
+echo ""
+
+if [[ "$TEST_SUITE" == "pydicom_master" ]]; then
+    wget https://github.com/pydicom/pydicom/archive/master.tar.gz -O /tmp/pydicom.tar.gz
+    tar xzf /tmp/pydicom.tar.gz
+    pip install $PWD/pydicom-master
+    python -c "import pydicom; print('pydicom version', pydicom.__version__)"
+elif [[ "$TEST_SUITE" == "pydicom_release" ]]; then
+    pip install pydicom
+    python -c "import pydicom; print('pydicom version', pydicom.__version__)"
+fi
+
+python --version
