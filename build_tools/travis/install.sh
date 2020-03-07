393135cb43d4115da515a69137c3feb345c60e5a
scaramallion <scaramallion@users.noreply.github.com>
2020-03-07 12:15:46 +1100

    Simplify pydicom master installation

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -4,23 +4,20 @@ set -e
 
 pip install coverage
 pip install pytest-cov
-pip install pytest
+pip install -U pytest
 pip install asv
-#python setup.py install
 
 echo ""
 echo "Test suite is " $TEST_SUITE
 echo ""
 
 if [[ "$TEST_SUITE" == "pydicom_master" ]]; then
-    wget https://github.com/pydicom/pydicom/archive/master.tar.gz -O /tmp/pydicom.tar.gz
-    tar xzf /tmp/pydicom.tar.gz
-    pip install $PWD/pydicom-master
-    python -c "import pydicom; print('pydicom version', pydicom.__version__)"
+    pip install git+https://github.com/pydicom/pydicom.git
 elif [[ "$TEST_SUITE" == "pydicom_release" ]]; then
     pip install pydicom
-    python -c "import pydicom; print('pydicom version', pydicom.__version__)"
 fi
 
 python --version
+python -c "import pydicom; print('pydicom version', pydicom.__version__)"
+python -c "import pytest; print('pytest version', pytest.__version__)"
 python -c "import ssl; print(ssl.OPENSSL_VERSION)"
