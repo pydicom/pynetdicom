2702a13a01966177d96aff1c14643aa27639e97a
alcir <alciregi@posteo.net>
2023-07-20 10:02:39 +0200

    Update hide_modules.py
    Remove deprecated importlib.abc.Finder

--- pynetdicom/tests/hide_modules.py
+++ pynetdicom/tests/hide_modules.py
@@ -30,7 +30,7 @@ try:
     import importlib.abc
 
     # py>=3.3 has MetaPathFinder
-    _ModuleHiderBase = getattr(importlib.abc, "MetaPathFinder", importlib.abc.Finder)
+    _ModuleHiderBase = getattr(importlib.abc, "MetaPathFinder")
 except ImportError:
     # py2
     _ModuleHiderBase = object
